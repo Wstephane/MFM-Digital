49e7afd51d6553af46d8bf855edc63502e1a779c
Stéphane <stewyns.pro@gmail.com>
2019-01-08 17:35:11 +0100

    ajout page site maintenance The Restaurant

---
+++ page_siteMaintenance_therestaurant/index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Page de redirection</title>
+    <link type="text/css" rel="stylesheet" href="//fonts.googleapis.com/css?family=Open+Sans+Condensed:300,700%7CLora" id="avia-google-webfont">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+    <style>
+        body {
+            font-family: "Open Sans Condensed", sans-serif;
+            text-align: center;
+        }   
+        .logo {
+            background-color: black;
+            padding-top: 5px;
+        }
+        img {
+            max-width: 100%;
+        }
+        .box {
+            background-color: #f5f5f5;
+            padding-top: 75px;
+            padding-bottom: 75px;
+        }
+        .title {
+            margin-bottom: 25px;
+        }
+        .text {
+            font-size: 120%;
+        }
+        p {
+            margin-top: 25px;
+        }
+        .social-media {
+            background-color: black;
+            padding-top: 35px;
+            padding-bottom: 35px;
+        }
+        i {
+            margin-right: 35px;
+            margin-left: 35px;
+        }
+        a:visited, a:link {
+            color: #aa9443;
+        }
+        a:hover {
+            color: #f00;
+        }
+    </style>
+</head>
+<body>
+    <section>
+        <div class="logo">
+            <img src="logo1.jpg" alt="logo" width="400px">
+        </div>
+        <div class="box">
+            <div class="title">
+                <h2>Site Maintenance</h2>
+            </div>  
+            <div class="text">
+                <i class="fas fa-exclamation-triangle fa-4x"></i>   
+                <p>We sincerely apologize for the inconvenience. </br>
+                Our site is currently undergoing scheduled </br>
+                maintenance and upgrades, but will return shortly. </br>
+                Thank you for you patience.</p>
+            </div>
+        </div>
+        <div>
+            <div class="social-media">
+                <a href="https://www.facebook.com/therestaurantpb" target="_blank"><i class="fab fa-facebook-f fa-2x"></i><a>
+                <a href="https://twitter.com/therestaurantpb" target="_blank"><i class="fab fa-twitter fa-2x"></i></a>
+                <a href="https://www.instagram.com/therestaurantbypb/" target="_blank"><i class="fab fa-instagram fa-2x"></i></a> 
+            </div>
+        </div>
+    </section>
+</body>
+</html>
